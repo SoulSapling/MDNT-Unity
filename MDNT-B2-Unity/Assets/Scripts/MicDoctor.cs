5a5f86977f62b8e3006b78110b59daa16a013356
AntCri <73044415+AntCri@users.noreply.github.com>
2020-10-25 16:20:36 +1100

    Added mic scripts

---
+++ MDNT-B2-Unity/Assets/Scripts/MicDoctor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MicDoctor : MonoBehaviour
+{
+    // Get list of Microphone devices and print the names to the log
+    void Start()
+    {
+        foreach (var device in Microphone.devices)
+        {
+            Debug.Log("Name: " + device);
+        }
+    }
+}
---
+++ MDNT-B2-Unity/Assets/Scripts/MicrophoneInput.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class MicrophoneInput : MonoBehaviour {
+	//public AudioSource audio;
+	public GameObject btn_record;
+	private AudioSource aud;
+
+	void Start(){
+		GetComponent<AudioSource> ().loop = true;
+	}
+
+	public void StartMicrophone () {
+		//GetComponent<AudioSource>().clip =  Microphone.Start("RODECaster Pro Multichannel", true, 5, 44100);
+        GetComponent<AudioSource>().clip = Microphone.Start("Built-in Microphone", true, 5, 44100);
+        GetComponent<AudioSource> ().loop = true;
+	}
+
+	public void StopMicrophone () {
+		//Microphone.End("RODECaster Pro Multichannel");
+        Microphone.End("Built-in Microphone");
+        //Stops the recording of the device	
+    }
+	public void playRecord () {
+		GetComponent<AudioSource>().Play();//Stops the audio
+		GetComponent<AudioSource> ().loop = true;
+
+	}
+	public void stopRecord () {
+		GetComponent<AudioSource>().Stop ();
+
+		
+	}
+
+
+}
