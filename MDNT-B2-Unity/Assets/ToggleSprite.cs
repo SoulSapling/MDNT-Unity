b0f5a947d687cd367ad4e66483372bd20523728d
SoulSapling <haquearman.ms@gmail.com>
2020-11-16 15:48:18 +1100

    Drum Sprites swapped in. Made Swap sprite script to toggle sprites

---
+++ MDNT-B2-Unity/Assets/ToggleSprite.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ToggleSprite : MonoBehaviour
+{
+
+    public Toggle targetToggle;
+    public Sprite onSprite;
+    public Sprite offSprite;
+    public Image targetBackground;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (targetToggle.isOn == true)
+        {
+            targetBackground.sprite = offSprite;
+        } else
+        {
+            targetBackground.sprite = onSprite;
+        }
+    }
+}
