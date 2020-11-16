ebbad66df1faf1281a4462eee9fa857cf18f87c0
SoulSapling <haquearman.ms@gmail.com>
2020-11-16 16:56:41 +1100

    He Dance :smile:  (only to drums atm)

---
+++ MDNT-B2-Unity/Assets/CharController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour
+{
+
+    public AudioSource[] drumsAudioObjects;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (drumsAudioObjects[0].mute == false || drumsAudioObjects[1].mute == false || drumsAudioObjects[2].mute == false || drumsAudioObjects[3].mute == false || drumsAudioObjects[4].mute == false)
+        {
+            anim.SetBool("drumsOn", true);
+            Debug.Log("mute is off");
+        }
+        else
+        {
+            anim.SetBool("drumsOn", false);
+            Debug.Log("mute is on");
+        }
+    }
+}
